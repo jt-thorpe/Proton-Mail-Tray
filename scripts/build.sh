c1162df3d5e7e1906677b25d618caaa53ba9954a
jt-thorpe <jtt-github@proton.me>
2024-08-23 13:04:23 +0100

    chore: update build script to include /configs

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,4 +12,6 @@ pyinstaller --onefile \
             --name "ProtonMailTray" \
             --add-data "resources/icon/proton-mail.png:resources/icon" \
             --add-data "logs:logs" \
+            --add-data "configs/config.json:configs" \
+            --add-data "configs/logging_config.json:configs" \
             --additional-hooks-dir "scripts/hook-proton_mail_tray.py" proton_mail_tray/app.py
